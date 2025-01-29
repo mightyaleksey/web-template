13e6b05808f641f011fc610ad949bef30077f067
Aleksey Litvinov <iamhoneyboy@gmail.com>
2025-01-29 12:31:50 +0000

    support bourne shell and add patch

--- scripts/bin.sh
+++ scripts/bin.sh
@@ -1,13 +1,13 @@
-#!/bin/zsh
+#!/bin/sh
 
-__projectname=$(realpath $0 | xargs dirname | xargs dirname)
-__workingname=$(pwd)
+__templates_dir=$(realpath $0 | xargs dirname | xargs dirname)
+__working_dir=$(pwd)
 
 # directory to copy files to
 destname="${1:-tmp}"
 selected_template=""
 
-templates=($(ls "$__projectname/templates"))
+templates=($(ls "$__templates_dir/templates"))
 
 PS3="Select web template to apply:"
 select option in ${templates[@]}; do
@@ -18,19 +18,29 @@ select option in ${templates[@]}; do
 done
 
 # create project directory
-if [ ! -d "$__workingname/$destname" ]; then
-  echo "create $destname"
-  mkdir -p "$__workingname/$destname"
+if [ ! -d "$__working_dir/$destname" ]; then
+  echo "create $destname/"
+  mkdir -p "$__working_dir/$destname"
 fi
 
 # directories
-template_dir="$__projectname/templates/$selected_template"
-project_dir="$__workingname/$destname"
+template_dir="$__templates_dir/templates/$selected_template"
+project_dir="$__working_dir/$destname"
 
 # file, source directory
 function copy_file() {
-  echo "create $destname/$1"
-  cp -r "$2/$1" "$project_dir/"
+  if [[ $(dirname "$2/$1") != "$2" ]]; then
+    local folder=$(dirname $1)
+
+    echo "create $destname/$folder/"
+    mkdir -p "$project_dir/$folder"
+
+    echo "create $destname/$1"
+    cp -r "$2/$1" "$project_dir/$folder/"
+  else
+    echo "create $destname/$1"
+    cp -r "$2/$1" "$project_dir/"
+  fi
 }
 
 # copy template files
@@ -39,8 +49,9 @@ for f in $(ls "$template_dir"); do
 done
 
 # copy generic files
-copy_file ".gitignore" "$__projectname"
-copy_file ".vscode" "$__projectname"
+copy_file ".gitignore" "$__templates_dir"
+copy_file ".vscode" "$__templates_dir"
+copy_file "scripts/patch.sh" "$__templates_dir"
 
 # update package name
 sed -i '' -e "s/web-template/$(basename $destname)/" "$project_dir/package.json"
---
+++ scripts/patch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+__project_dir=$(realpath $0 | xargs dirname | xargs dirname)
+
+function fix_jsx_props_no_multi_spaces() {
+  local filepath="node_modules/eslint-plugin-react/lib/rules/jsx-props-no-multi-spaces.js"
+
+  if [[ -f "$__project_dir/$filepath" ]]; then
+    local line=$(sed '112!d' "$__project_dir/$filepath")
+
+    if [[ $line = "      return nodeTypeArguments.type === 'TSTypeParameterInstantiation';" ]]; then
+      sed -i '' "112s/      return nodeTypeArguments.type === 'TSTypeParameterInstantiation';/      return nodeTypeArguments?.type === 'TSTypeParameterInstantiation';/" "$__project_dir/$filepath"
+    fi
+  fi
+}
+
+fix_jsx_props_no_multi_spaces
